4778862c9801b958570a23297fa74906cc6ea34f
Yusuke Wada <yusuke@kamawada.com>
2025-08-10 05:30:00 +0900

    feat: support Cloudflare Images (#12)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { Hono } from 'hono/quick'
+import { Hono } from 'hono/tiny'
 import { cache } from 'hono/cache'
 import { sha256 } from 'hono/utils/crypto'
 import { basicAuth } from 'hono/basic-auth'
 import { getExtension } from 'hono/utils/mime'
+import * as z from 'zod'
 
 const maxAge = 60 * 60 * 24 * 30
 
@@ -40,14 +41,48 @@ app.get(
   })
 )
 
+const imageParameterSchema = z.object({
+  width: z.coerce.number().optional(),
+  height: z.coerce.number().optional(),
+  quality: z.coerce.number().optional()
+})
+
+const getPreferredContentType = (acceptHeader: string | undefined, fallback: string) => {
+  if (acceptHeader) {
+    const types = ['image/avif', 'image/webp']
+    for (const type of types) {
+      if (acceptHeader.includes(type)) {
+        return type
+      }
+    }
+  }
+  return fallback
+}
+
 app.get('/:key', async (c) => {
   const key = c.req.param('key')
 
   const object = await c.env.BUCKET.get(key)
   if (!object) return c.notFound()
-  const data = await object.arrayBuffer()
   const contentType = object.httpMetadata?.contentType ?? ''
 
+  if (c.env.IMAGES) {
+    const schemaResult = imageParameterSchema.safeParse(c.req.query())
+    if (schemaResult.success) {
+      const preferredContentType = getPreferredContentType(c.req.header('Accept'), contentType)
+      const parameters = schemaResult.data
+      const imageResult = await c.env.IMAGES.input(object.body).transform(parameters).output({
+        //@ts-expect-error the contentType maybe valid format
+        format: preferredContentType,
+        quality: parameters.quality
+      })
+      const res = imageResult.response()
+      res.headers.set('Cache-Control', `public, max-age=${maxAge}`)
+      return res
+    }
+  }
+
+  const data = await object.arrayBuffer()
   return c.body(data, 200, {
     'Cache-Control': `public, max-age=${maxAge}`,
     'Content-Type': contentType
